371e77f67835dc8f353d3e602f243b5a250e235e
Claudiaborze <claudia.borze@yahoo.com>
2024-10-14 12:43:19 +0300

    Create and add splash screen animation

--- recipes_app/App.js
+++ recipes_app/App.js
@@ -2,25 +2,38 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
 import CategoriesScreen from './screens/CategoriesScreen';
 import SearchScreen from './screens/SearchScreen';
+import SplashScreen from './screens/SplashScreen';
+import { enableScreens } from 'react-native-screens';
+
+//Enable native screens for better performance
+enableScreens();
 
 const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
 
+//Drawer Navigator for main screens
+function DrawerNavigator () {
+  return (
+    <Drawer.Navigator screenOptions={{headerShown: false,}}>
+    <Drawer.Screen name="Home" component={HomeScreen} />
+    <Drawer.Screen name="Categories" component={CategoriesScreen} />
+    <Drawer.Screen name="Search" component={SearchScreen} />
+  </Drawer.Navigator>
+  );
+}
+
+//Stack Navigator for SplashScreen and DrawerNavigator
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          headerShown: true, 
-        }}
-      >
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Categories" component={CategoriesScreen} />
-        <Drawer.Screen name="Search" component={SearchScreen} />
-      </Drawer.Navigator>
+      <Stack.Navigator initialRouteName="Splash" screenOptions={{headerShown:false}}>
+        <Stack.Screen name="Splash" component={SplashScreen} />
+        <Stack.Screen  name="MainApp" component={DrawerNavigator} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
---
+++ recipes_app/screens/SplashScreen.js
@@ -0,0 +1,74 @@
+import React, {useEffect} from "react";
+import {View,Text,Image,StyleSheet} from 'react-native';
+import Animated, {useSharedValue,useAnimatedStyle,withTiming,withDelay} from "react-native-reanimated";
+
+const SplashScreen = ({navigation}) => {
+    
+    //Values for animation
+    const scaleValue = useSharedValue(0);
+    const opacityValue = useSharedValue(0);
+
+    useEffect(() => {
+
+        //Start animation
+        scaleValue.value = withTiming(1, {duration:1000});
+        opacityValue.value = withDelay(500,withTiming(1, {duration:1000}));
+
+        //Navigate to HomeScreen after the animation completes
+        const time = setTimeout(() => {
+            navigation.replace('MainApp'); //Navigate to MainApp (DrawerNavigator)
+        }, 3000);
+        return () => clearTimeout(time);
+    }, [navigation]);
+
+    //Animated styles
+    const animatedIcon = useAnimatedStyle(() => {
+        return {
+            transform: [{ scale: scaleValue.value }],
+        };
+    });
+
+    const animatedText = useAnimatedStyle(() => {
+        return {
+            opacity: opacityValue.value,
+        };
+    });
+
+    return (
+        <View style={styles.container}>
+            <Animated.Image source={require('../assets/foodIcon.png')}
+            style= {[styles.icon, animatedIcon]} />
+            <Animated.Text style={[styles.appName, animatedText]}>YumPlate</Animated.Text>
+            <Animated.Text style={[styles.tagText, animatedText]}>Discover New Flavors</Animated.Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:'#6CBB3C'
+    },
+    icon: {
+        width:180,
+        height:180,
+        marginBottom:15,
+    },
+
+    appName: {
+        fontSize:38,
+        color:'#000000',
+        fontFamily:'Roboto',
+        fontWeight:'bold'
+    },
+    tagText: {
+        fontSize: 14,
+        color:'#000000',
+        fontWeight:'bold'
+    },
+});
+
+export default SplashScreen; 
+
